e2c404486c6d775b77953134dbe59a002fff86c1
germanalvarez8 <ger.22.alvarez@gmail.com>
2020-11-24 16:55:12 -0300

    fix cantidad de argumentos

--- verify_pr_lables.py
+++ verify_pr_lables.py
@@ -34,7 +34,7 @@ def get_env_var(env_var_name, echo_value=False):
     return value
 
 # Check if the number of input arguments is correct
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     raise ValueError('Invalid number of arguments!')
 
 # Get the GitHub token
